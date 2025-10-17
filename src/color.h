c6615ef3e0840654b52b384945527a41426a96cb
maallaard <64651989+maallaard@users.noreply.github.com>
2025-10-16 20:47:59 -0400

    complete chapter 4

--- src/color.h
+++ src/color.h
@@ -2,16 +2,31 @@
 #define COLOR_H
 
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 
 using color = vec3;
 
+color ray_color(const ray &r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    double a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
+void write_header(std::ostream &out, const int &image_width, const int &image_height)
+{
+    out << "P3\n";
+    out << image_width << ' ' << image_height << '\n';
+    out << "255\n";
+}
+
 void write_color(std::ostream &out, const color &pixel_color)
 {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
+    double r = pixel_color.x();
+    double g = pixel_color.y();
+    double b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
     int rbyte = int(255.999 * r);
--- src/main.cc
+++ src/main.cc
@@ -1,32 +1,79 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
 int main()
 {
-    // Image
+    /* Image */
 
-    int image_width = 256;
-    int image_height = 256;
+    // Set aspect ratio to 16:9.
+    double aspect_ratio = 16.0 / 9.0;
 
-    // Render
+    // Set image width to 400 pixels.
+    int image_width = 400;
 
-    std::cout << "P3\n";
-    std::cout << image_width << ' ' << image_height << '\n';
-    std::cout << "255\n";
+    // Calculate image height based on it's width.
+    int image_height = int(image_width / aspect_ratio);
 
+    // Ensure image height is at least 1.
+    if (image_height < 1)
+    {
+        image_height = 1;
+    }
+
+    /* Viewport */
+
+    // Set focal length.
+    double viewport_focal_length = 1.0;
+
+    // Set viewport height.
+    double viewport_height = 2.0;
+
+    // Calculate viewport width based on it's height.
+    double viewport_width = viewport_height * (double(image_width) / image_height);
+
+    // Calculate vectors across the horizontal and down the vertical viewport edges.
+    vec3 viewport_u = vec3(viewport_width, 0, 0);
+    vec3 viewport_v = vec3(0, -viewport_height, 0);
+
+    // Set viewport's center coordinates.
+    vec3 viewport_center = vec3(0, 0, 0);
+
+    // Calculate horizontal and vertical delta vectors from pixel to pixel.
+    vec3 pixel_delta_u = viewport_u / image_width;
+    vec3 pixel_delta_v = viewport_v / image_height;
+
+    // Calculate location of top-leftmost pixel.
+    vec3 viewport_top_left = viewport_center - vec3(0, 0, viewport_focal_length) - (viewport_u / 2) - (viewport_v / 2);
+
+    vec3 pixel00_loc = viewport_top_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    /* Render */
+
+    // Write .ppm header information.
+    write_header(std::cout, image_width, image_height);
+
+    // Iterate through each pixel left-to-right and top-to-bottom.
     for (int j = 0; j < image_height; j++)
     {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 
         for (int i = 0; i < image_width; i++)
         {
-            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            vec3 pixel_center = pixel00_loc + (pixel_delta_u * i) + (pixel_delta_v * j);
 
+            vec3 ray_direction = pixel_center - viewport_center;
+            ray r(viewport_center, ray_direction);
+
+            // Set pixel color for current pixel.
+            color pixel_color = ray_color(r);
+
+            // Write pixel RGB bytes to .ppm file.
             write_color(std::cout, pixel_color);
         }
     }
 
-    std::clog << "\rDone.\n";
+    std::clog << "\rDone.                 \n";
 }
